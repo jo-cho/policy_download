5871f98838cbbf272d69dee74d2eb38c078d5e90
JH Cho <jhcho1016@naver.com>
2023-12-15 19:41:04 +0900

    add

--- crawling_epic.py
+++ crawling_epic.py
@@ -1,20 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-def prev_article():
-    href = [i.get_attribute('href') for i in driver.find_elements(By.CSS_SELECTOR,'dd > a[onclick^="javascript:fn_beforeAfter"]')][-1]
-    driver.get(href)
+#def prev_article():
+#    href = [i.get_attribute('href') for i in driver.find_elements(By.CSS_SELECTOR,'dd > a[onclick^="javascript:fn_beforeAfter"]')][-1]
+ #   driver.get(href)
 
 
 if __name__ == '__main__':
     driver = webdriver.Chrome()
     driver.get("https://eiec.kdi.re.kr/policy/materialList.do")
+    for n in range(1,21):
+        # get the first article
+        driver.find_element(By.CSS_SELECTOR,
+            f'#ui_contents > div.page_conts-bar.policy_material > div#listArea > ul > li:nth-child({n}) > a').click()\
 
-    # get the first article
-    driver.find_element(By.CSS_SELECTOR,
-        '#ui_contents > div.page_conts-bar.policy_material > div#listArea > ul > li:nth-child(1) > a').click()\
-    #j = 1 # 파일 번호 카운터
-    while True:
+        driver.find_element(By.CSS_SELECTOR,
+            'div.view_top.downbtn_org > div.bot > div.downbtn_line > div.download_wrap').click()\
+
         try:
             pdf_link = \
                 [i.get_attribute("href") for i in driver.find_elements(By.CSS_SELECTOR,
@@ -27,4 +29,6 @@ if __name__ == '__main__':
                 pass
         except IndexError:
             pass
-        #prev_article()
+
+        driver.find_element(By.CSS_SELECTOR,
+                            'div.view_botm > a').click()

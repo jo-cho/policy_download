6898d5c73d9fc436002186f0e8859feeef805270
JH Cho <jhcho1016@naver.com>
2023-12-22 10:07:30 +0900

    update

--- crawling_epic_new.py
+++ crawling_epic_new.py
@@ -1,18 +1,29 @@
 from selenium import webdriver
+from selenium.common.exceptions import UnexpectedAlertPresentException
+from selenium.webdriver.support.ui import WebDriverWait
 
-num_start = 246126 #오늘꺼 시작
-num_end = 246188 #오늘꺼 마지막
+num_start = 246167 #오늘꺼 시작
+num_end = 246187 #오늘꺼 마지막
 
+def every_downloads_chrome(driver):
+    if not driver.current_url.startswith("chrome://downloads"):
+        driver.get("chrome://downloads/")
+    return driver.execute_script("""
+        var items = document.querySelector('downloads-manager')
+            .shadowRoot.getElementById('downloadsList').items;
+        if (items.every(e => e.state === "COMPLETE"))
+            return items.map(e => e.fileUrl || e.file_url);
+        """)
 
 if __name__ == '__main__':
     driver = webdriver.Chrome()
     for n in range(num_start, num_end+1):
+        driver.implicitly_wait(10)
         try:
-            pdf_link = f"https://eiec.kdi.re.kr/policy/callDownload.do?num={n}&filenum=1"
-            driver.get(pdf_link)
-            if driver.page_source == '<html><head></head><body></body></html>':
-                driver.back()
-            else:
-                pass
-        except IndexError:
+            link = f"https://eiec.kdi.re.kr/policy/callDownload.do?num={n}&filenum=1"
+            driver.get(link)
+            driver.implicitly_wait(5)
+        except UnexpectedAlertPresentException:
             pass
+    paths = WebDriverWait(driver, 150, 1).until(every_downloads_chrome)
+    print(paths)
